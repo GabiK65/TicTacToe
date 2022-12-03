ef3f1ef520c812b352fc9dca6970b255c75259ad
GabiK65 <alex.paulics@proton.me>
2022-12-03 17:05:28 +0100

    "Optimized" code Moved from cmake to meson Updated README.md

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
 # A simple game of Tic-Tac-Toe 
-#### Version:0.4.0
+#### Version:0.5.0
 
 ---
 ## ToDo:
 - ✅ ~~Add "play again" option~~ 
 - ✅ ~~Add "character" selection~~
 - ✅ ~~Add "AI" for singleplayer mode~~
+- 🚧 Optimize code
+- Add online multiplayer mode
+- Add a nice gui
+- Support for input devices (keyboard, mouse, gamepad)
 - Make "AI" decide the best next step
 - Add game difficulty
-- 🚧 Add a nice gui (version 0.5.0)
-- 🚧 Support for input devices (keyboard, mouse, gamepad) (version 0.5.0)
-- Add online multiplayer mode
 
 ## How to build and run:
 
 ### Installing required dependencies:
-- Debian and Ubuntu based systems:
-```sudo apt update && sudo apt install build-essential cmake``` 
-- ArchLinux and other Arch based systems:
-```sudo pacman -Syu base-devel cmake```
+```
+pip3 install --user meson
+```
 
 ### Building and running the game
 ```
@@ -26,13 +26,15 @@ git clone https://github.com/GabiK65/TicTacToe.git
 
 cd ./TicTacToe
 
-cmake .
+meson setup build
 
-cmake --build .
+cd build
 
-chmod a+x ./build/TicTacToe
+meson compile
 
-./build/TicTacToe
+chmod a+x ./src/TicTacToe # Optional
+
+./src/TicTacToe
 ```
 
-- all built executables can be found in the 'build' directory 
+- all built executables can be found in the 'build/src' directory
--- src/main.cpp
+++ src/main.cpp
@@ -2,59 +2,60 @@
 #include <cstring>
 #include <ctime>
 #include <clocale>
+#include <array>
+#include <cctype>
 
 using namespace std;
 
-bool draw, game_exit = false;
-char board[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}}, play_again, player1, player2;
-int i, j, choice, row, column, turn = 1, gamemode = 0, temp_int;
+const int BOARD_SIZE = 9;
+
+bool game_over = false;
+array <char, BOARD_SIZE> board;
+int i, choice, turn = 1, gamemode = 0;
+char play_again, player1, player2;
 
 void characterChoice() {
 	cout << "Choose a character to play as [A-Z]" << endl;
 	do {
 		cout << "Player one: ";
 		cin >> player1;
+		if (islower(player1)) {
+		    player1 = toupper(player1);
+		}
 	} while (player1 < 'A' or player1 > 'Z');
 	do {
 		cout << "Player two: ";
 		cin >> player2;
-	} while ((player2 < 'A' or player2 > 'Z') or player1 == player2);
+		if (islower(player2)) {
+		    player2 = toupper(player2);
+		}
+	} while ((player1 < 'A' or player1 > 'Z') and player1 == player2);
 }
 
 void displayBoard() {
-	cout << "\t  " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;
+	cout << "\t  " << board[0] << " | " << board[1] << " | " << board[2] << endl;
 	cout << "\t ---|---|---" << endl;
-	cout << "\t  " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
+	cout << "\t  " << board[3] << " | " << board[4] << " | " << board[5] << endl;
 	cout << "\t ---|---|---" << endl;
-	cout << "\t  " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
+	cout << "\t  " << board[6] << " | " << board[7] << " | " << board[8] << endl;
 	cout << endl;
 }
 
 void playerChoice() {
-	cout << "Player: " << turn << " Choose a cell: " << endl;
-	cin >> choice;
+    do {
+	    cout << "Player: " << turn << " Choose a cell: " << endl;
+	    cin >> choice;
 
-	if (cin.fail()){
-			cout << "Thats not a number!\nPlease try again!" << endl;
-			// clear error state
-			cin.clear();
-			// discard 'bad' character(s)
-			cin.ignore(1000, '\n');
-	}
 
-	switch(choice) {
-		case 1: row = 0; column = 0; break;
-		case 2: row = 0; column = 1; break;
-		case 3: row = 0; column = 2; break;
-		case 4: row = 1; column = 0; break;
-		case 5: row = 1; column = 1; break;
-		case 6: row = 1; column = 2; break;
-		case 7: row = 2; column = 0; break;
-		case 8: row = 2; column = 1; break;
-		case 9: row = 2; column = 2; break;
-		default:
-			cout << "Invalid cell!\nPlease try again" << endl;
-	};
+	    if (cin.fail()){
+			    cout << "Thats not a number!\nPlease try again!" << endl;
+			    // clear error state
+			    cin.clear();
+			    // discard 'bad' character(s)
+			    cin.ignore(1000, '\n');
+	    }
+    } while (choice < 1 or choice > 9);
+    choice --;
 }
 
 void playerVSplayer() {
@@ -62,24 +63,24 @@ void playerVSplayer() {
 
 	if(turn == 1 ) {
 		//player one
-		if(board[row][column] != player1 and board[row][column] != player2)
+		if(board[choice] != player1 and board[choice] != player2)
 		{
-			board[row][column] = player1;
+			board[choice] = player1;
 			displayBoard();
 			turn = 2;
-		} else if (board[row][column] == player1 or board[row][column] == player2) {
-				cout << "Cell " << board[row][column] << " already occupied \nPlease try again!" << endl; 
+		} else if (board[choice] == player1 or board[choice] == player2) {
+				cout << "Cell " << board[choice] << " already occupied \nPlease try again!" << endl;
 				turn = 1;
 		}
 	} else if(turn == 2 ) {
 		//player two
-		if(board[row][column] != player1 and board[row][column] != player2)
+		if(board[choice] != player1 and board[choice] != player2)
 		{
-			board[row][column] = player2;
+			board[choice] = player2;
 			displayBoard();
 			turn = 1;
-		} else if (board[row][column] == player1 or board[row][column] == player2) {
-				cout << "Cell " << board[row][column] << " already occupied \nPlease try again!" << endl; 
+		} else if (board[choice] == player1 or board[choice] == player2) {
+				cout << "Cell " << board[choice] << " already occupied \nPlease try again!" << endl;
 				turn = 2;
 		}
 	}
@@ -92,59 +93,74 @@ void playerVSAI() {
 	if(turn == 1 ) {
 		playerChoice();
 
-		if(board[row][column] != player1 and board[row][column] != player2)
+		if(board[choice] != player1 and board[choice] != player2)
 		{
-			board[row][column] = player1;
+			board[choice] = player1;
 			displayBoard();
 			turn = 2;
-		} else if (board[row][column] == player1 or board[row][column] == player2) {
-				cout << "Cell " << board[row][column] << " already occupied \nPlease try again!" << endl;
+		} else if (board[choice] == player1 or board[choice] == player2) {
+				cout << "Cell " << board[choice] << " already occupied \nPlease try again!" << endl;
 				turn = 1;
 		}
 	//"AI"
 	} else if(turn == 2 ) {
-		row = rand() % 3; column = rand() % 3;
+		choice = rand() % 9;
 		
-		if(board[row][column] != player1 and board[row][column] != player2)
+		if(board[choice] != player1 and board[choice] != player2)
 		{
-			board[row][column] = player2;
+			board[choice] = player2;
 			displayBoard();
 			turn = 1;
-		} else if (board[row][column] == player1 or board[row][column] == player2) {
+		} else if (board[choice] == player1 or board[choice] == player2) {
 			turn = 2;
 		}
 	}
 }
 
-bool inGame() {
-	//check for complete line(s) 
-	for(i = 0; i < 3; i++) {
-		//check for rows and columns
-		if ((board[i][0] == board[i][1] and board[i][0] == board[i][2]) or (board[0][i] == board[1][i] and board[0][i] == board[2][i])) {
-			return false;
-		}
-		//check for diagonal
-		if ((board[0][0] == board[1][1] and board[0][0] == board[2][2]) or (board[0][2] == board[1][1] and board[0][2] == board[2][0])) {
-			return false;
-		}
-	}
+bool checkWin() {
+    // Check for horizontal wins
+    for (i = 0; i < BOARD_SIZE; i += 3) {
+        if (board[i] == board[i + 1] and board[i + 1] == board[i + 2]) {
+        return true;
+        }
+    }
 
-    for (int i=0; i<3; i++) {
-    	for(int j=0; j<3; j++){
-    		if(board[i][j] != player1 && board[i][j] != player2)
-   		return true;
-		}
-	}
+    // Check for vertical wins
+    for (i = 0; i < 3; i++) {
+        if (board[i] == board[i + 3] and board[i + 3] == board[i + 6]) {
+        return true;
+        }
+    }
 
-	draw = true;
-	return false;
+    // Check for diagonal wins
+    if ((board[0] == board[4] and board[4] == board[8]) or
+        (board[2] == board[4] and board[4] == board[6])) {
+        return true;
+    }
+
+    // No wins found
+    return false;
+}
+
+bool checkDraw() {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        if (board[i] != 'X' and board[i] != 'O') {
+            // There is at least one empty cell, so the game is not a draw
+            return false;
+        }
+    }
+
+    // All cells are occupied, so the game is a draw
+    return true;
 }
 
 int main() {
 	srand(time(NULL));
 	setlocale(LC_ALL, "");
+    // initialize board
+	for (i = 0; i < BOARD_SIZE; i++){board[i] = static_cast<char>(i + 49);}
 
-	while (game_exit == false) {
+	while (!game_over) {
 		while (gamemode == 0) {
 			cout << "Select a gamemode: \n\t1 - Singleplayer against AI \n\t2 - Multiplayer against another person" << endl;
 			cin >> gamemode;
@@ -155,48 +171,51 @@ int main() {
 				// discard 'bad' character(s)
 				cin.ignore(1000, '\n');
 			}
-			if (gamemode < 1 or gamemode > 2) {gamemode = 0;} else if (gamemode >= 1 and gamemode <= 2) {characterChoice();displayBoard();}
+			if (gamemode < 1 or gamemode > 2) {
+		        gamemode = 0;
+	        } else if (gamemode >= 1 and gamemode <= 2) {
+			characterChoice();
+			displayBoard();
+			}
 		}
 
-		while (inGame() == true) {
-			if(gamemode == 1) {playerVSAI();} else if(gamemode == 2) {playerVSplayer();}
-			inGame();
+		while (!checkWin()) {
+			if (gamemode == 1) {playerVSAI();} else if (gamemode == 2) {playerVSplayer();}
+			checkWin();
+			checkDraw();
 		}
 		
-		if (turn == 2 and draw == false) {
+
+		if (turn == 2 and checkWin() and !checkDraw()) {
 			cout << "Game Over! \n\t Player 1 won!" << endl;
-		} else if (turn == 1 and draw == false) {
+			game_over = true;
+		} else if (turn == 1 and checkWin() and !checkDraw()) {
 			cout << "Game Over! \n\t Player 2 won!" << endl;
-		} else {
-			cout << "It's a draw! Nobody wins" << endl;
+			game_over = true;
+		} else if (checkDraw()) {
+		    cout << "Game Over! \n\t It's a draw!" << endl;
+		    game_over = true;
 		}
 
-		do {
-			cout << "Play again? \nY or N" << endl;
-			cin >> play_again;
-			if(play_again == 'y' or play_again == 'Y') {
-				gamemode = 0; game_exit = false;
-				temp_int = 0;
-				for(i = 0; i < 3; i++) {
-					for (j = 0; j < 3; j++) {
-						temp_int++;
-						switch (temp_int) {
-							case 1: board[i][j] = '1'; break;
-							case 2: board[i][j] = '2'; break;
-							case 3: board[i][j] = '3'; break;
-							case 4: board[i][j] = '4'; break;
-							case 5: board[i][j] = '5'; break;
-							case 6: board[i][j] = '6'; break;
-							case 7: board[i][j] = '7'; break;
-							case 8: board[i][j] = '8'; break;
-							case 9: board[i][j] = '9'; break;
-						}
-					}
-				}
-				turn = 1;
-			}
-			if(play_again == 'n' or play_again == 'N') {gamemode = 0; game_exit = true;} else {cout << "Invalid choice!" << endl;}
-		} while (gamemode != 0);
-	}
-	return 0;
+	    do {
+	        cout << "Do you want to play again? (Y/N)" << endl;
+		    cin >> play_again;
+
+            if(islower(play_again)) {
+                play_again = toupper(play_again);
+            }
+
+		    if (play_again == 'Y') {
+		        // start the game again
+			    // reset essential variables
+			    gamemode = 0; game_over = false; turn = 1;
+			    // reset board
+			    for (i = 0; i < BOARD_SIZE; i++){board[i] = static_cast<char>(i + 49);}
+		    } else if (play_again == 'N') {
+                cout << "Thank you for playing!" << endl;
+                return 0;
+            }
+        } while (gamemode != 0);
+    }
+    return 0;
 }
